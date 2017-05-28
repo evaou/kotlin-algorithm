ab96ec756e56b304eae693fb45ac632cb39abcb2
evaou <speeder528@gmail.com>
2017-05-28 12:24:11 +0800

    Add Merge Sort program

--- merge-sort/src/MergeSort.kt
+++ merge-sort/src/MergeSort.kt
@@ -1,3 +1,20 @@
+import java.io.File
+
+fun readFile(filename: String): IntArray {
+    val regex = Regex("\\s")
+    val list = File(filename).readText().split(regex)
+    val listSize = list.size
+    val data = IntArray(listSize)
+
+    for (i in list.indices) {
+        data[i] = Integer.parseInt(list[i])
+    }
+
+    return data
+}
+
 fun main(args: Array<String>) {
+    var data = readFile("input.txt")
+
     println("Hello, Merge Sort!")
 }
